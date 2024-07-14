9797e5cca2a0f1901ff836ca949ff214de6ce29e
Darren Chua <ddschua001@mymail.sim.edu.sg>
2024-07-15 00:02:09 +0800

    Commenting out sample

--- db_schema.sql
+++ db_schema.sql
@@ -6,17 +6,17 @@ BEGIN TRANSACTION;
 
 -- Create your tables with SQL commands here (watch out for slight syntactical differences with SQLite vs MySQL)
 
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_name TEXT NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS users (
+--     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_name TEXT NOT NULL
+-- );
 
-CREATE TABLE IF NOT EXISTS email_accounts (
-    email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email_address TEXT NOT NULL,
-    user_id  INT, --the user that the email account belongs to
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+-- CREATE TABLE IF NOT EXISTS email_accounts (
+--     email_account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     email_address TEXT NOT NULL,
+--     user_id  INT, --the user that the email account belongs to
+--     FOREIGN KEY (user_id) REFERENCES users(user_id)
+-- );
 
 CREATE TABLE IF NOT EXISTS articles (
     article_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -56,13 +56,13 @@ INSERT INTO settings (id, blog_title, author_name) VALUES (1, 'My Blog', 'Author
 
 -- Insert default data (if necessary here)
 
-INSERT INTO users ('user_name') VALUES ('Simon Star');
-INSERT INTO users ('user_name') VALUES ('Dianne Dean');
-INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
+-- INSERT INTO users ('user_name') VALUES ('Simon Star');
+-- INSERT INTO users ('user_name') VALUES ('Dianne Dean');
+-- INSERT INTO users ('user_name') VALUES ('Harry Hilbert');
 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
-INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
+-- INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@gmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('simon@hotmail.com', 1); 
+-- INSERT INTO email_accounts ('email_address', 'user_id') VALUES ('dianne@yahoo.co.uk', 2); 
 
 COMMIT;
 
